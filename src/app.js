7e7cc35f37a80f2b5e4e97ba79cc0370a93c1f3c
Ruben Maher <ruben@maher.fyi>
2018-09-19 12:50:53 +0930

    src/app: make listen address configurable
    If you have an ipv6 capable server, express may bind to ::: rather than
    0.0.0.0.

--- src/app.js
+++ src/app.js
@@ -13,6 +13,6 @@ var app = express();
 app.use(bodyParser.json());
 app.use('/api/v1', router);
 
-app.listen(config.server.port, function() {
-	winston.info('Listening on localhost:' + config.server.port);
+app.listen(config.server.port, config.server.host, function() {
+	winston.info('Listening on ' + config.server.host + ':' + config.server.port);
 });
